1ee7ac5181b87e992e5fb83ff397bd52fa9a96bc
borelli.19312 <marco05.borelli@gmail.com>
2022-05-25 13:27:04 +0200

    pusha Checcooooooo. Introdotta aggiunta classe. Se se ne elimina una in mezzo e poi si aggiunge gli indici si sballano

--- Borelli_VotiFileNuovo/Form2.Designer.cs
+++ Borelli_VotiFileNuovo/Form2.Designer.cs
@@ -31,20 +31,22 @@
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // treeView1
             // 
             this.treeView1.Location = new System.Drawing.Point(12, 5);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(240, 419);
+            this.treeView1.Size = new System.Drawing.Size(256, 435);
             this.treeView1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 430);
+            this.button1.Location = new System.Drawing.Point(12, 446);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 32);
+            this.button1.Size = new System.Drawing.Size(123, 32);
             this.button1.TabIndex = 1;
             this.button1.Text = "ELIMINA CLASSE";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,26 +54,49 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(138, 430);
+            this.button2.Location = new System.Drawing.Point(145, 446);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(114, 32);
+            this.button2.Size = new System.Drawing.Size(123, 32);
             this.button2.TabIndex = 2;
             this.button2.Text = "AGGIUNGI CLASSE";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 484);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(224, 483);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(44, 20);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "OK";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(264, 477);
+            this.ClientSize = new System.Drawing.Size(280, 516);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.treeView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
+            this.DoubleClick += new System.EventHandler(this.Form2_DoubleClick);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -80,5 +105,7 @@
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
--- Borelli_VotiFileNuovo/Form2.cs
+++ Borelli_VotiFileNuovo/Form2.cs
@@ -7,13 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 using System.IO;
+using Microsoft.VisualBasic;
 
 namespace Borelli_VotiFileNuovo
 {
     public partial class Form2 : Form
     {
-        string riga;
+        string riga, nuovaClasse="";
 
         public Form2()
         {
@@ -22,19 +24,75 @@ namespace Borelli_VotiFileNuovo
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            textBox1.Visible = false;
+            button3.Visible = false;
             using (StreamWriter write = new StreamWriter(@"./tmp.txt", true)) { }
 
             using (StreamReader read = new StreamReader(@"./tmp.txt"))
             {
-                while ((riga = read.ReadLine()) != "^")
+                while ((riga = read.ReadLine()) != "^" && riga != null)
                     treeView1.Nodes.Add(riga.Substring(5, riga.Length - 5));
             }
         }
+
+       
 
         private void button1_Click(object sender, EventArgs e) //rimuovi classe
         {
             EliminazioneInFile(@"./tmp.txt", @"./tmp1.txt", treeView1);
             treeView1.SelectedNode.Remove();
+        }
+        private void button2_Click(object sender, EventArgs e) //aggiungi classe
+        {
+            textBox1.Visible = true;
+            button3.Visible = true;
+        }
+        private void button3_Click(object sender, EventArgs e)//ok in aggiunta classe
+        {
+            nuovaClasse += textBox1.Text.ToUpper();
+            string nuovaRiga = OttieniIndiceFile(treeView1.GetNodeCount(true));
+            //MessageBox.Show($"{OttieniIndiceFile(treeView1.GetNodeCount(true))}");
+            //OttieniIndiceAlbero(,)
+            AggiuntaClasseFile( @"./tmp1.txt", @"./tmp.txt",treeView1, nuovaRiga, nuovaClasse);
+            treeView1.Nodes.Add(nuovaClasse);
+            textBox1.Visible = false;
+            button3.Visible = false;
+            textBox1.Text = "";
+        }
+
+        public static void AggiuntaClasseFile(string fileTemp, string fileOrig, TreeView albero, string nuovoNumeroRiga, string nomeClasse)
+        {
+            string riga;
+            using (StreamWriter write = new StreamWriter(fileTemp)) { }
+            using (StreamReader read = new StreamReader(fileOrig))
+            {
+                nuovoNumeroRiga = nuovoNumeroRiga.Substring(0, 5);
+                int numRiga = Convert.ToInt16(nuovoNumeroRiga);
+
+                for (int i=0;i< numRiga; i++)
+                {
+                    riga = read.ReadLine();
+                    using (StreamWriter write = new StreamWriter(fileTemp, true))
+                        write.WriteLine(riga);
+                } //mi fermo a scrivere l'indice prima di inserire il nuovo
+            }
+            using (StreamWriter write = new StreamWriter(fileTemp, true))
+            {
+                write.WriteLine($"{nuovoNumeroRiga} {nomeClasse}"); //scrivo il nuovo e poi chiudo
+                write.WriteLine("^");
+            }
+
+            
+            SovrascrivereFile(fileTemp, fileOrig);
+        }
+
+        public static void SovrascrivereFile(string fileTemp, string fileOrig)
+        {
+            FileInfo fi = new FileInfo(fileTemp);
+            FileInfo newFi = new FileInfo(fileOrig);
+            newFi.Delete();
+            newFi = fi.CopyTo(fileOrig);
+            fi.Delete();
         }
 
         public static void EliminazioneInFile(string fileOrig, string fileTemp, TreeView albero)
@@ -45,7 +103,7 @@ namespace Borelli_VotiFileNuovo
 
             using (StreamReader read = new StreamReader(fileOrig))
             {
-                while ((riga = read.ReadLine()) != "^")
+                while ((riga = read.ReadLine()) != "^") //continuo a copiare finche non arrivo al ^. Poi lo aggiungo
                 {
                     if (riga.Substring(0, 5) != OttieniIndiceFile(OttieniIndiceAlbero(albero.SelectedNode.Text, albero)))
                     {
@@ -56,13 +114,9 @@ namespace Borelli_VotiFileNuovo
                 using (StreamWriter write = new StreamWriter(fileTemp, true))
                     write.WriteLine("^");
             }
+            SovrascrivereFile(fileTemp, fileOrig);
 
-            FileInfo fi = new FileInfo(fileTemp);
-            FileInfo newFi = new FileInfo(fileOrig);
-            newFi.Delete();
-            newFi = fi.CopyTo(fileOrig);
 
-            fi.Delete();
         }
 
         public static int OttieniIndiceAlbero(string nome, TreeView albero)
@@ -84,7 +138,13 @@ namespace Borelli_VotiFileNuovo
             }
             return indice;
         }
-        public static string OttieniIndiceFile(int posizione)
+
+        private void Form2_DoubleClick(object sender, EventArgs e)
+        {
+            //OttieniIndiceAlbero
+        }
+
+        public static string OttieniIndiceFile(int posizione) //mi aggiunge tutti gli zeri per salvarlo su file
         {
             string pos = posizione.ToString();
             if (pos.Length == 5)
@@ -100,4 +160,6 @@ namespace Borelli_VotiFileNuovo
             return "-1";
         }
     }
+
 }
+
