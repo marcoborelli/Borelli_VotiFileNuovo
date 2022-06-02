0b81bbb06ecd6e1aae7db93893ca9c8026e8f5ee
Gianpinetto <marco05.borelli@gmail.com>
2022-06-02 14:37:51 +0200

    primi tentativi di aggiunta di data e voto, decisione della strategia.

--- Borelli_VotiFileNuovo/Form5.Designer.cs
+++ Borelli_VotiFileNuovo/Form5.Designer.cs
@@ -29,28 +29,23 @@
         private void InitializeComponent()
         {
             this.button3 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(224, 487);
+            this.button3.Location = new System.Drawing.Point(235, 487);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(44, 20);
+            this.button3.Size = new System.Drawing.Size(33, 20);
             this.button3.TabIndex = 19;
             this.button3.Text = "OK";
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 488);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(183, 20);
-            this.textBox1.TabIndex = 18;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -60,6 +55,7 @@
             this.button2.TabIndex = 17;
             this.button2.Text = "AGGIUNGI VOTO";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -80,14 +76,29 @@
             this.dataGridView1.Size = new System.Drawing.Size(256, 420);
             this.dataGridView1.TabIndex = 20;
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 487);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(162, 20);
+            this.dateTimePicker1.TabIndex = 21;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(180, 487);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(49, 20);
+            this.textBox1.TabIndex = 22;
+            // 
             // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(280, 516);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -103,9 +114,10 @@
         #endregion
 
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Borelli_VotiFileNuovo/Form5.cs
+++ Borelli_VotiFileNuovo/Form5.cs
@@ -21,21 +21,41 @@ namespace Borelli_VotiFileNuovo
         
         private void Form5_Load(object sender, EventArgs e)
         {
-            //dataGridView1.Rows.Add("HELLO", "HELLO1");
             //dataGridView1.SelectedRows.;
             tabella.Columns.Add("HELO", typeof(double));
             tabella.Columns.Add("HELLOOO", typeof(string));
 
+            tabella.Rows.Add(10, "HELLO1");
+            tabella.Rows.Add(101, "HELLO11");
+            tabella.Rows.Add(102, "HELLO12");
+            tabella.Rows.Add(103, "HELLO13");
+
             dataGridView1.DataSource = tabella;
-           // tabella.Select();
+            dataGridView1.AllowUserToAddRows = false;
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) //elimina voto
         {
+            int i= dataGridView1.CurrentCell.RowIndex;
+            tabella.Rows.RemoveAt(i);
             //DataRow ciao = new DataRow(45);
-            //tabella.Rows.RemoveAt(tabella.Rows.IndexOf(ciao);
             //dataGridView1.Dele
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //dataGridView1.AllowUserToAddRows = true;
+            //dataGridView1.AllowUserToAddRows = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)//aggiungi voto
+        {
+            DateTime helo = dateTimePicker1.Value;
+            string heloo = $"{helo.Day}/{helo.Month}/{helo.Year}";
+            
+            tabella.Rows.Add(textBox1.Text,heloo);
+            //MessageBox.Show(heloo);
         }
     }
 }
